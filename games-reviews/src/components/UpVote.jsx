a28e83ebafc06f2f06ad91cf4165d0566ef147d2
Kev Morel <kev.morel@northcoders.com>
2021-11-23 18:58:24 +0000

    adds content to review card

---
+++ games-reviews/src/components/UpVote.jsx
@@ -0,0 +1,7 @@
+export default function UpVote() {
+  return (
+    <div>
+      <h3>UPVOTER</h3>
+    </div>
+  );
+}
--- games-reviews/src/pages/Reviews.jsx
+++ games-reviews/src/pages/Reviews.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import FancyCard from "../components/FancyCard";
+import UpVote from "../components/UpVote";
 import { getReviews } from "../utils/apiCall";
 
 export default function Reviews() {
@@ -19,9 +20,21 @@ export default function Reviews() {
       <h2>Check these reviews...</h2>
 
       {reviews.map((review) => {
+        console.log(review);
         return (
-          <FancyCard>
-            <h3>{review.title}</h3>
+          <FancyCard key={review.title}>
+            <div className="review-sub-one">
+              <img
+                src={review.review_img_url}
+                alt={review.title}
+                className="review-image"
+              />
+              <div className="review-sub-two">
+                <h3>VOTES: {review.votes}</h3>
+                <UpVote />
+              </div>
+            </div>
+            <h4>{review.title}</h4>
           </FancyCard>
         );
       })}
