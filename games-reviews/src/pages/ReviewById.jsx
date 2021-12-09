c436715744d92a7ce61dea971fd9aa211c1ee06f
Kev Morel <kev.morel@northcoders.com>
2021-12-09 15:12:51 +0000

    cleans up reviewById component

--- games-reviews/src/pages/ReviewById.jsx
+++ games-reviews/src/pages/ReviewById.jsx
@@ -7,10 +7,10 @@ import UpVote from "../components/UpVote";
 import {
   getReviewById,
   getCommentsByReviewId,
-  postComment,
   deleteCommentById,
   deleteReviewById,
 } from "../utils/apiCall";
+import CommentForm from "../components/CommentForm";
 
 export default function ReviewById({ reviews, setReviews }) {
   const { user } = useContext(UserContext);
@@ -18,7 +18,6 @@ export default function ReviewById({ reviews, setReviews }) {
   const [commentCount, setCommentCount] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
   const [comments, setComments] = useState([]);
-  const [addComment, setAddComment] = useState("");
   const [wasDeletePressed, setWasDeletePressed] = useState(false);
   const { reviewId } = useParams();
 
@@ -113,40 +112,13 @@ export default function ReviewById({ reviews, setReviews }) {
           </div>
         )}
       </FancyCard>
-      <form className="comment-input">
-        <input
-          id="comment-textbox"
-          type="text"
-          placeholder="Leave your comment here..."
-          value={addComment}
-          onChange={(e) => setAddComment(e.target.value)}
-        />
-        <button
-          className="post-button"
-          onClick={(e) => {
-            e.preventDefault();
-            const commentToInsert = {
-              username: user.username,
-              body: addComment,
-            };
-            const newComments = [commentToInsert, ...comments];
-            const newCommentCount = (
-              Number(review.comment_count) + 1
-            ).toString();
-            setCommentCount(newCommentCount);
-            postComment(reviewId, commentToInsert)
-              .then((res) => {
-                setAddComment("");
-              })
-              .catch((err) => {
-                console.log(err);
-              });
-            setComments(newComments);
-          }}
-        >
-          POST
-        </button>
-      </form>
+      <CommentForm
+        reviewId={reviewId}
+        review={review}
+        comments={comments}
+        setComments={setComments}
+        setCommentCount={setCommentCount}
+      />
       {comments.map((comment) => {
         const deleteCommentId = `delete-comment-${comment.comment_id}`;
 
