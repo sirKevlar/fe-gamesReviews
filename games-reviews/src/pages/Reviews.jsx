1e464b3a467b06e5c36bf2db87bdd87721842419
Kev Morel <kev.morel@northcoders.com>
2021-12-09 14:53:27 +0000

    cleans up reviews component

--- games-reviews/src/pages/Reviews.jsx
+++ games-reviews/src/pages/Reviews.jsx
@@ -6,6 +6,8 @@ import UpVote from "../components/UpVote";
 import ReviewForm from "../components/ReviewForm";
 import AddReviewButton from "../components/AddReviewButton";
 import { getReviews, getCategories } from "../utils/apiCall";
+import SideNavContentsTop from "../components/SideNavContentsTop";
+import SideNavContentsBottom from "../components/SideNavContentsBottom";
 
 export default function Reviews({
   reviews,
@@ -39,152 +41,19 @@ export default function Reviews({
   return (
     <main>
       <SideNav>
-        <div className="sidenav-top">
-          {categories.map((category) => {
-            return (
-              <div
-                onClick={(e) => {
-                  setSelectedCategory(category.slug);
-                  getReviews(category.slug).then((reviews) => {
-                    setReviews(reviews);
-                  });
-                }}
-                className="category"
-                value={category.slug}
-                id={category.slug}
-                key={category.slug}
-              >
-                <h6 className="slug">{category.slug}</h6>
-              </div>
-            );
-          })}
-        </div>
-        <div className="sidenav-bottom white">
-          <div className="sidenav-bottom-top">
-            <h5 className="white sort-title">Sort By:</h5>
-            <div className="sort-radio">
-              <div className="sort-radio-left">
-                <div className="radio-pairs">
-                  <input
-                    onChange={({ target: { value } }) => {
-                      setSortBy(value);
-                      getReviews(selectedCategory, value, order).then(
-                        (reviewsFromApi) => {
-                          setReviews(reviewsFromApi);
-                        }
-                      );
-                    }}
-                    type="radio"
-                    name="sort-by"
-                    value="title"
-                  />
-                  <label className="radio-label" htmlFor="title">
-                    TITLE
-                  </label>
-                </div>
-                <div className="radio-pairs">
-                  <input
-                    onChange={({ target: { value } }) => {
-                      setSortBy(value);
-                      getReviews(selectedCategory, value, order).then(
-                        (reviewsFromApi) => {
-                          setReviews(reviewsFromApi);
-                        }
-                      );
-                    }}
-                    type="radio"
-                    name="sort-by"
-                    value="votes"
-                  />
-                  <label className="radio-label" htmlFor="votes">
-                    VOTES
-                  </label>
-                </div>
-              </div>
-              <div className="sort-radio-right">
-                <div className="radio-pairs">
-                  <input
-                    onChange={({ target: { value } }) => {
-                      setSortBy(value);
-                      getReviews(selectedCategory, value, order).then(
-                        (reviewsFromApi) => {
-                          setReviews(reviewsFromApi);
-                        }
-                      );
-                    }}
-                    type="radio"
-                    name="sort-by"
-                    value="owner"
-                  />
-                  <label className="radio-label" htmlFor="owner">
-                    OWNER
-                  </label>
-                </div>
-                <div className="radio-pairs">
-                  <input
-                    onChange={({ target: { value } }) => {
-                      setSortBy(value);
-                      getReviews(selectedCategory, value, order).then(
-                        (reviewsFromApi) => {
-                          setReviews(reviewsFromApi);
-                        }
-                      );
-                    }}
-                    type="radio"
-                    name="sort-by"
-                    value="created_at"
-                  />
-                  <label className="radio-label" htmlFor="created">
-                    CREATED
-                  </label>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="sidenav-bottom-bottom">
-            <h5 id="order-by" className="white sort-title">
-              Order By:
-            </h5>
-            <div className="sort-radio">
-              <div className="radio-pairs">
-                <input
-                  onChange={({ target: { value } }) => {
-                    setOrder(value);
-                    getReviews(selectedCategory, sortBy, value).then(
-                      (reviewsFromApi) => {
-                        setReviews(reviewsFromApi);
-                      }
-                    );
-                  }}
-                  type="radio"
-                  name="order"
-                  value="asc"
-                />
-                <label className="radio-label" htmlFor="asc">
-                  ASC
-                </label>
-              </div>
-              <div className="radio-pairs">
-                <input
-                  onChange={({ target: { value } }) => {
-                    setOrder(value);
-                    getReviews(selectedCategory, sortBy, value).then(
-                      (reviewsFromApi) => {
-                        setReviews(reviewsFromApi);
-                      }
-                    );
-                  }}
-                  type="radio"
-                  name="order"
-                  value="desc"
-                />
-                <label className="radio-label" htmlFor="desc">
-                  DESC
-                </label>
-              </div>
-            </div>
-          </div>
-        </div>
+        <SideNavContentsTop
+          categories={categories}
+          setSelectedCategory={setSelectedCategory}
+          setReviews={setReviews}
+        />
+        <SideNavContentsBottom
+          sortBy={sortBy}
+          setSortBy={setSortBy}
+          order={order}
+          setOrder={setOrder}
+          selectedCategory={selectedCategory}
+          setReviews={setReviews}
+        />
       </SideNav>
       <h2>Reviews...</h2>
       <FancyCard>
